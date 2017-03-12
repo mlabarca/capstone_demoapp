081e97d284266a858446c434f1821e1fe87a1865
mlabarca <mauriciolabarca4tw@gmail.com>
2017-03-12 12:41:21 -0300

    Added thing tag component with dummy data

--- app/assets/javascripts/spa-demo/subjects/things/things.component.js
+++ app/assets/javascripts/spa-demo/subjects/things/things.component.js
@@ -11,11 +11,18 @@
       },
       require: {
         thingsAuthz: "^sdThingsAuthz"
-      }      
+      }
     })
     .component("sdThingSelector", {
       templateUrl: thingSelectorTemplateUrl,
       controller: ThingSelectorController,
+      bindings: {
+        authz: "<"
+      }
+    })
+    .component("sdThingTagSelector", {
+      templateUrl: thingTagSelectorTemplateUrl,
+      controller: ThingTagSelectorController,
       bindings: {
         authz: "<"
       }
@@ -26,18 +33,23 @@
   thingEditorTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
   function thingEditorTemplateUrl(APP_CONFIG) {
     return APP_CONFIG.thing_editor_html;
-  }    
+  }
   thingSelectorTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
   function thingSelectorTemplateUrl(APP_CONFIG) {
     return APP_CONFIG.thing_selector_html;
-  }    
+  }
+  thingTagSelectorTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
+  function thingTagSelectorTemplateUrl(APP_CONFIG) {
+    return APP_CONFIG.thing_tag_selector_html;
+  }
+
 
   ThingEditorController.$inject = ["$scope","$q",
                                    "$state","$stateParams",
                                    "spa-demo.authz.Authz",
                                    "spa-demo.subjects.Thing",
                                    "spa-demo.subjects.ThingImage"];
-  function ThingEditorController($scope, $q, $state, $stateParams, 
+  function ThingEditorController($scope, $q, $state, $stateParams,
                                  Authz, Thing, ThingImage) {
     var vm=this;
     vm.create = create;
@@ -49,10 +61,10 @@
 
     vm.$onInit = function() {
       console.log("ThingEditorController",$scope);
-      $scope.$watch(function(){ return Authz.getAuthorizedUserId(); }, 
-                    function(){ 
+      $scope.$watch(function(){ return Authz.getAuthorizedUserId(); },
+                    function(){
                       if ($stateParams.id) {
-                        reload($stateParams.id); 
+                        reload($stateParams.id);
                       } else {
                         newResource();
                       }
@@ -68,7 +80,7 @@
     }
 
     function reload(thingId) {
-      var itemId = thingId ? thingId : vm.item.id;      
+      var itemId = thingId ? thingId : vm.item.id;
       console.log("re/loading thing", itemId);
       vm.images = ThingImage.query({thing_id:itemId});
       vm.item = Thing.get({id:itemId});
@@ -76,8 +88,8 @@
       vm.images.$promise.then(
         function(){
           angular.forEach(vm.images, function(ti){
-            ti.originalPriority = ti.priority;            
-          });                     
+            ti.originalPriority = ti.priority;
+          });
         });
       $q.all([vm.item.$promise,vm.images.$promise]).catch(handleError);
     }
@@ -86,12 +98,12 @@
         var ti=vm.images[i];
         if (ti.toRemove || ti.originalPriority != ti.priority) {
           return true;
-        }        
+        }
       }
       return false;
-    }    
+    }
 
-    function create() {      
+    function create() {
       vm.item.errors = null;
       vm.item.$save().then(
         function(){
@@ -103,10 +115,10 @@
 
     function clear() {
       newResource();
-      $state.go(".",{id: null});    
+      $state.go(".",{id: null});
     }
 
-    function update() {      
+    function update() {
       vm.item.errors = null;
       var update=vm.item.$update();
       updateImageLinks(update);
@@ -118,7 +130,7 @@
       angular.forEach(vm.images, function(ti){
         if (ti.toRemove) {
           promises.push(ti.$remove());
-        } else if (ti.originalPriority != ti.priority) {          
+        } else if (ti.originalPriority != ti.priority) {
           promises.push(ti.$update());
         }
       });
@@ -126,15 +138,15 @@
       console.log("waiting for promises", promises);
       $q.all(promises).then(
         function(response){
-          console.log("promise.all response", response); 
+          console.log("promise.all response", response);
           //update button will be disabled when not $dirty
           $scope.thingform.$setPristine();
-          reload(); 
-        }, 
-        handleError);    
+          reload();
+        },
+        handleError);
     }
 
-    function remove() {      
+    function remove() {
       vm.item.$remove().then(
         function(){
           console.log("thing.removed", vm.item);
@@ -146,14 +158,14 @@
     function handleError(response) {
       console.log("error", response);
       if (response.data) {
-        vm.item["errors"]=response.data.errors;          
-      } 
+        vm.item["errors"]=response.data.errors;
+      }
       if (!vm.item.errors) {
         vm.item["errors"]={}
-        vm.item["errors"]["full_messages"]=[response]; 
-      }      
+        vm.item["errors"]["full_messages"]=[response];
+      }
       $scope.thingform.$setPristine();
-    }    
+    }
   }
 
   ThingSelectorController.$inject = ["$scope",
@@ -165,13 +177,39 @@
 
     vm.$onInit = function() {
       console.log("ThingSelectorController",$scope);
-      $scope.$watch(function(){ return Authz.getAuthorizedUserId(); }, 
-                    function(){ 
+      $scope.$watch(function(){ return Authz.getAuthorizedUserId(); },
+                    function(){
                       if (!$stateParams.id) {
-                        vm.items = Thing.query();        
+                        vm.items = Thing.query();
                       }
                     });
     }
+    return;
+    //////////////
+  }
+
+  ThingTagSelectorController.$inject = ["$scope",
+                                        "$stateParams",
+                                        "spa-demo.authz.Authz",
+                                        "spa-demo.subjects.Thing"];
+  function ThingTagSelectorController($scope, $stateParams, Authz, Thing) {
+    var vm=this;
+    var dummyTags = [
+      { text: 'Tag1' },
+      { text: 'Tag2' },
+      { text: 'Tag3' },
+      { text: 'Tag4' },
+      { text: 'Tag5' },
+      { text: 'Tag6' }
+    ];
+    vm.tags = [
+      { text: 'Tag1' },
+      { text: 'Tag2' }
+    ];
+
+    vm.loadTags = function(query) {
+      return dummyTags;
+    };
     return;
     //////////////
   }
