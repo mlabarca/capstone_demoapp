a56bc99277a71c23b6aab645f98b7677bbe11f34
mlabarca <mauriciolabarca4tw@gmail.com>
2017-03-19 12:59:38 -0300

    Move tag json to jbuilder reusable tempalte

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,9 +2,8 @@ class TagsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    tags = Tag.all
-    tags = tags.where('name ILIKE ?', "%#{params[:term]}%") if params[:term].present?
-    render json: tags.as_json, status: :ok
+    @tags = Tag.all
+    @tags = @tags.where('name ILIKE ?', "%#{params[:term]}%") if params[:term].present?
   end
 
 
